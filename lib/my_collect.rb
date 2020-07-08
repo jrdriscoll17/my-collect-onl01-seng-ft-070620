2444f5082fc3b50f13991ce807d8e7e59f7cc8b3
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-08 00:37:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   i = 0
   
   while i < array.length
-    modified_array.push(yield(array[i]))
+    yield(array[i])
+    modified_array.push(array[i])
     i += 1
   end
   modified_array
